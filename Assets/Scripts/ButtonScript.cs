350e2cf196961b4ddc5f12fa5da96db15e4f84db
Lorelei Basken <loreleinbasken@gmail.com>
2017-10-25 15:24:21 -0400

    Added DeathTrigger, ButtonScript, and added new scenes

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Kayak")
+        {
+            Destroy(other.gameObject);
+
+            //Application.LoadLevel("End");
+        }
+    }
+}
